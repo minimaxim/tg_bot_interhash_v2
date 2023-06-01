051cd8684f056fb4aab6d9d18731411b98549c95
Maxim <maxxim.zhurkin99@gmail.com>
2023-06-01 23:52:57 +0300

    added new coins and inline

---
+++ keyboards/inline/users/coins.py
@@ -0,0 +1,29 @@
+from itertools import zip_longest
+
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from keyboards.inline.users.general import UserCallbackData
+from models import Coin
+
+
+async def coin_ikb() -> InlineKeyboardMarkup:
+    coins = await Coin.all(is_published=True)
+    coins_iter = iter(coins)
+    coins_iter = map(list, zip_longest(*([coins_iter]*2)))
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=coin.name.upper(),
+                callback_data=UserCallbackData(
+                    target='formilize',
+                    action='get',
+                    coin_id=coin.id
+                ).pack()
+            )
+            for coin in line
+            if coin
+        ]
+        for line in coins_iter
+    ]
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
