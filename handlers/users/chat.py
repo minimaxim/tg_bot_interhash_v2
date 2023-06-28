3378bd3759c4b2d83a3e2b476c989f0f0a58f3a9
Maxim <maxxim.zhurkin99@gmail.com>
2023-06-28 20:33:27 +0300

    v2.3 changes from client (added simple chat)

---
+++ handlers/users/chat.py
@@ -0,0 +1,21 @@
+from aiogram import Router, F
+from aiogram.enums import ChatType
+from aiogram.types import Message
+
+from loader import CHAT_ID, bot
+
+user_chat_router = Router(name='user_chat')
+
+
+@user_chat_router.message(F.chat.type == ChatType.PRIVATE)
+async def forward(message: Message):
+    await message.forward(chat_id=CHAT_ID)
+
+
+
+@user_chat_router.message(F.chat.type == ChatType.SUPERGROUP, F.reply_to_message)
+async def answer(message: Message):
+    await bot.send_message(
+        chat_id=message.reply_to_message.forward_from.id,
+        text=message.text,
+    )
