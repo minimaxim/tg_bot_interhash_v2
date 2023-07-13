9b11e71a6640638f5cbfed84822cc063a60f8755
Maxim <maxxim.zhurkin99@gmail.com>
2023-07-13 22:41:59 +0300

    final version new chat

--- handlers/users/chat.py
+++ handlers/users/chat.py
@@ -9,7 +9,15 @@ user_chat_router = Router(name='user_chat')
 
 @user_chat_router.message(F.chat.type == ChatType.PRIVATE)
 async def forward(message: Message):
-    await message.forward(chat_id=CHAT_ID)
+
+    await message.forward(chat_id=CHAT_ID,)
+
+    username = message.from_user.username
+
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text=f"Сообщение от пользователя @{username}"
+    )
 
 
 @user_chat_router.message(F.chat.type == ChatType.SUPERGROUP, F.reply_to_message)
